2cfd48e6422eb43b9926b851dc81b98b50b39b3d
Thiago88pe <thiago_alves88@hotmail.com>
2024-05-14 00:35:25 -0300

    feat: criando opcoes

--- main.py
+++ main.py
@@ -9,3 +9,27 @@ LIMITE = 500
 extrato = ''
 numero_saques = 0
 LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(MENU)
+
+    if opcao == 'd':
+        deposito = float(input('Informe o valor para depósito: '))
+
+    elif opcao == 's':
+        saque = float(input('Informe o valor do saque: '))
+
+    elif opcao == 'e':
+        print(' Extrato '.center(35, '-'))
+
+    elif opcao == 'q':
+        print('-'.center(35, '-'))
+        print('Saindo...')
+        print('-'.center(35, '-'))
+        break
+
+    else:
+        print('-'.center(35, '-'))
+        print('Opção inválida! Tente novamente.')
+        print('-'.center(35, '-'))
